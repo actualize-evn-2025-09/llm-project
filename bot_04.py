34fe716e424c0a026ad297d7ecdd08710226820a
leonshimizu <lmshimizu@gmail.com>
2025-12-31 10:09:42 +1000

    Implement the pinecone uploader

---
+++ bot_04.py
@@ -0,0 +1,10 @@
+# Instead of including all the documention - only include the relevant sections of the documentation
+
+# search engine technology
+
+# RAG - Retrieval Augmented Generation
+
+# Pinecone
+# semantic search
+# embeddings - converting the words into numbers
+
